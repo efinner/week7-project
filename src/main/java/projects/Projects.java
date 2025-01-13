84a22867c5a703b391a07e5649998781442b8ecf
efinner <efinner14@gmail.com>
2025-01-13 14:33:23 -0700

    Initial commit

---
+++ src/main/java/projects/Projects.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package projects;
+
+import projects.dao.DbConnection;
+
+/**
+ * 
+ */
+public class Projects {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DbConnection.getConnection();
+	}
+
+}
